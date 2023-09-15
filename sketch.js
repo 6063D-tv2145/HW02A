7701428c37571b46a82d60af54b7cb16a10412cf
Thao Chi Vu <tv2145@nyu.edu>
2023-09-15 09:42:03 -0400

    added two triangles, square and corresponding lines

--- sketch.js
+++ sketch.js
@@ -1,5 +1,16 @@
 function setup() {
+    createCanvas(832, 832)
+    background(247, 230, 204)
+    trianglesize=280
+    triangleEnd=400
 }
 
 function draw() {
+    fill(0)
+    triangle(0, 0, 0, trianglesize, triangleEnd, trianglesize)
+    triangle(832, 832, 832, 832-trianglesize, 832-triangleEnd, 832-trianglesize)
+    quad(832-triangleEnd, 832-trianglesize, 550, 400, triangleEnd, trianglesize, width-550, height-400)
+    line(0, 832,width-550, height-400)
+    line(832, 0, 550, 400)
+
 }
